8e84edae57330bd388131517b2ec73ca7b569d6d
Hiba Anwar <hiba2anwar@gmail.com>
2026-01-26 18:30:19 +0400

    feat(camera): add camera calibration config

--- src/mach_hardware/launch/stereo_cameras.launch.py
+++ src/mach_hardware/launch/stereo_cameras.launch.py
@@ -1,54 +1,69 @@
+import os
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    # Install gscam: sudo apt install ros-humble-gscam
+    pkg_share = get_package_share_directory("mach_hardware")
+    
+    left_yaml = os.path.join(
+    	pkg_share,
+    	"config",
+    	"left.yaml"
+    )
+    
+    right_yaml = os.path.join(
+    	pkg_share,
+    	"config",
+    	"right.yaml"
+    )
 
     left_pipeline = (
-        "nvarguscamerasrc sensor_id=0 ! "
-        "video/x-raw(memory:NVMM),width=2304,height=1296,framerate=30/1,format=NV12 ! "
-        "nvvidconv flip-method=2 ! video/x-raw,width=1280,height=720,format=I420 ! "
+        "nvarguscamerasrc wbmode=0 sensor_id=1 ! "
+        "video/x-raw(memory:NVMM),width=1920,height=1080,framerate=30/1,format=NV12 ! "
+        "nvvidconv flip-method=2 ! video/x-raw,width=960,height=720,format=I420 ! "
         "videoconvert"
     )
 
     right_pipeline = (
-        "nvarguscamerasrc sensor_id=1 ! "
-        "video/x-raw(memory:NVMM),width=2304,height=1296,framerate=30/1,format=NV12 ! "
-        "nvvidconv flip-method=2 ! video/x-raw,width=1280,height=720,format=I420 ! "
+        "nvarguscamerasrc wbmode=0 sensor_id=0 ! "
+        "video/x-raw(memory:NVMM),width=1920,height=1080,framerate=30/1,format=NV12 ! "
+        "nvvidconv flip-method=2 ! video/x-raw,width=960,height=720,format=I420 ! "
         "videoconvert"
     )
 
     left_cam = Node(
         package="gscam",
-        executable="gscam",
+        executable="gscam_node",
         name="left_camera",
         output="screen",
         parameters=[{
             "gscam_config": left_pipeline,
             "frame_id": "left_camera_frame",
-            "camera_name": "left",
-            # "camera_info_url": [],
+            "camera_name": "left_camera",
+            "camera_info_url": "file://" + left_yaml,
         }],
         remappings=[
-            ("camera/image_raw", "left/image_raw"),
-            ("camera/camera_info", "left/camera_info"),
+            ("camera/image_raw", "camera/left/image_raw"),
+            ("camera/camera_info", "camera/left/camera_info"),
         ],
     )
 
     right_cam = Node(
         package="gscam",
-        executable="gscam",
+        executable="gscam_node",
         name="right_camera",
         output="screen",
         parameters=[{
             "gscam_config": right_pipeline,
             "frame_id": "right_camera_frame",
-            "camera_name": "right",
-            # "camera_info_url": [],
+            "camera_name": "right_camera",
+            "camera_info_url": "file://" + right_yaml,
         }],
         remappings=[
-            ("camera/image_raw", "right/image_raw"),
-            ("camera/camera_info", "right/camera_info"),
+            ("camera/image_raw", "camera/right/image_raw"),
+            ("camera/camera_info", "camera/right/camera_info"),
         ],
     )
 
